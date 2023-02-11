c548bef71f31be2a17cb20a0c21b1299a5368558
jmcdlungren <j.mcd.lungren@gmail.com>
2023-02-11 11:46:41 -0700

    working on local storage

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,12 +2,12 @@
 
 
 
-$(".btn").on("click", showNews); 
+$(".btn").on("click", showNews);
 
 function showNews() {
     var city = $("input").val();
 
-    
+
 
 
     var newsAPI = "ab3780e4108e48e2bdecf81068c21a51";
@@ -26,7 +26,7 @@ function showNews() {
             var titleOne = $(".event-title-1");
             var linkOne = $(".event-link-1");
             var infoOne = $(".event-info-1");
-            
+
             titleOne.text(data.articles[0].title)
             linkOne.attr('href', data.articles[0].url)
             infoOne.text(data.articles[0].description)
@@ -34,7 +34,7 @@ function showNews() {
             var titleTwo = $(".event-title-2");
             var linkTwo = $(".event-link-2");
             var infoTwo = $(".event-info-2");
-            
+
             titleTwo.text(data.articles[1].title)
             infoTwo.text(data.articles[1].description)
             linkTwo.attr('href', data.articles[1].url)
@@ -42,7 +42,7 @@ function showNews() {
             var titleThree = $(".event-title-3");
             var linkThree = $(".event-link-3");
             var infoThree = $(".event-info-3");
-            
+
             titleThree.text(data.articles[2].title)
             infoThree.text(data.articles[2].description)
             linkThree.attr('href', data.articles[2].url)
@@ -50,7 +50,7 @@ function showNews() {
             var titleFour = $(".event-title-4");
             var linkFour = $(".event-link-4");
             var infoFour = $(".event-info-4");
-            
+
             titleFour.text(data.articles[3].title)
             infoFour.text(data.articles[3].description)
             linkFour.attr('href', data.articles[3].url)
@@ -58,19 +58,48 @@ function showNews() {
             var titleFive = $(".event-title-5");
             var linkFive = $(".event-link-5");
             var infoFive = $(".event-info-5");
-            
+
             titleFive.text(data.articles[4].title)
             infoFive.text(data.articles[4].description)
             linkFive.attr('href', data.articles[4].url)
 
+
+
+            renderHistory();
         })
 
+    
 
-            
-              
+
+}
+
+
+
+function saveCity() {
+    var searchHistory = JSON.parse(localStorage.getItem("recent")) || []
+    searchHistory.push($("input").val())
+    localStorage.setItem("recent", JSON.stringify(searchHistory))
+}
+
+// function historySearch() {
+//     console.log(this.dataset.city)
+//     showNews(this.dataset.city)
+// }
+
+function renderHistory() {
+    var favorites = $(".favorites");
+    var history = JSON.parse(localStorage.getItem("recent")) || []
+    for (i = 0; i < history.length; i++) {
+        var recentSearch = document.createElement('button');
+        recentSearch.classList.add('btn')
+        recentSearch.setAttribute('data-city', history[i])
+        recentSearch.textContent = history[i]
+        favorites.append(recentSearch);
+
+        // recentSearch.addEventListener("click", historySearch)
+        
     }
+}
 
-
-
-
+renderHistory();
 
